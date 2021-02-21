651c7d0a6ce4d673ada235f99a938f8436c838c4
romyabbrederis <romy.abbrederis@gmail.com>
2021-02-21 16:35:48 +0100

    font style

--- pages/_app.js
+++ pages/_app.js
@@ -22,9 +22,14 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="shortcut icon" href="../romy-white-logo.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Montserrat:wght@100;200;400;500;600;700;800;900&family=Staatliches&display=swap"
+          rel="stylesheet"
+        />
+
         {/* <style>
           @import
-          url('https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Montserrat:wght@100;200;400;500;600;700;800;900&family=Staatliches&display=swap');
+          url('');
         </style> */}
       </Head>
       <Component {...pageProps} />
